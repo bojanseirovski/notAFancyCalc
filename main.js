93b6b1d171881d04c266aa62b8b2c3de57979751
Bojan Seirovski <druid0101@gmail.com>
2024-06-20 01:54:51 -0400

    styling

--- main.js
+++ main.js
@@ -8,48 +8,13 @@ const body = document.getElementsByTagName("body")[0];
 const content = document.getElementById("content");
 const opperate = document.getElementById("operate");
 
-
-body.addEventListener('dblclick', (event) => {
-    alert("Bojan Seirovski \251");
-});
-
 content.addEventListener('click', (event) => {
     const isButton = event.target.nodeName === 'BUTTON';
     if (!isButton) {
         return;
     }
     let op = event.target.getAttribute("data-opperand");
-    if (op != "=") {
-        //   number
-        if (!isNaN(op)) {
-            num = num + op;
-            opperate.value = num;
-        } else if (isOperator(op)) {
-            // operator
-            operations.push(parseInt(num));
-            operations.push(op);
-            num = "";
-            opperate.value = op;
-        }
-    } else if (op == "=") {
-        //calc
-        operations.push(parseFloat(num));
-        const newArray = operations.filter(function (value) {
-            return !Number.isNaN(value);
-        });
-        operations = newArray;
-        totalNums = calc(operations);
-        opperate.value = totalNums;
-        num = "";
-        operations = [parseFloat(totalNums)];
-    }
-    // clear
-    if (op == "C") {
-        num = "";
-        totalNums = "";
-        operations = [];
-        opperate.value = 0;
-    }
+    getOpperands(op);
 });
 
 function calc(ops) {
@@ -63,4 +28,44 @@ function calc(ops) {
 
 function isOperator(op) {
     return operators.indexOf(op) > -1;
+}
+
+function getOpperands(op) {
+    if (op != "=") {
+        //   number
+        if (!isNaN(op)) {
+            num = num + op;
+            opperate.value = num;
+        } else if (isOperator(op)) {
+            // operator
+            operations.push(parseInt(num));
+            operations.push(op);
+            num = "";
+            opperate.value = op;
+        }
+    } else if ((op == "=")) {
+        //calc
+        operations.push(parseFloat(num));
+        const newArray = operations.filter(function (value) {
+            return !Number.isNaN(value);
+        });
+        operations = newArray;
+        totalNums = calc(operations);
+        opperate.value = totalNums;
+        num = "";
+        operations = [parseFloat(totalNums)];
+    }
+    // clear
+    if (op == "C" || op == "c") {
+        num = "";
+        totalNums = "";
+        operations = [];
+        opperate.value = 0;
+    }
+}
+
+function isEnterPressed(key) {
+    if (key === 'Enter') {
+        return true;
+    }
 }
